f7bc751169c8223f913e33e6e7e6abde9ee8caf4
IndupuriMohan <152762951+IndupuriMohan@users.noreply.github.com>
2025-02-02 22:09:59 +0530

    urls.py

--- faq/urls.py
+++ faq/urls.py
@@ -2,10 +2,8 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import FAQViewSet
 
-# Create a router and register the FAQViewSet
 router = DefaultRouter()
 router.register(r'faqs', FAQViewSet, basename='faq')
 
-# Include the router URLs
 urlpatterns = [
     path('', include(router.urls)),
